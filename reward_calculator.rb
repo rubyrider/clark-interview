8b7d0e4845ae5809286518b55f008f9880055de2
Irfan Ahmed <irfandhk@gmail.com>
2022-06-01 08:15:26 +0200

    add: Invitation builder service and documentations

--- reward_calculator.rb
+++ reward_calculator.rb
@@ -3,9 +3,14 @@ require 'bundler/setup'
 
 Bundler.require
 
+# Auto loaders for the other services!
 autoload :InputFileHandler, "./input_file_handler.rb"
 autoload :Invitation, "./invitation"
 
+# The entry point service for this assignment
+#
+# Basically this calculator will take the input
+# and return the expected output
 class RewardCalculator
 
 end
